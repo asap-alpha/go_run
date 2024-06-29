273c6f91906b0434db5d3c265b8ea1db29d70875
Abdallah Mustapha <abdullahmustapha59@gmail.com>
2024-06-29 07:32:42 +0000

    adding interface implementation

--- enums.go
+++ enums.go
@@ -1,22 +1,35 @@
 package main
 
+import "fmt"
+
+// enum
+type enumType int
+
+const (
+	area enumType = iota
+	perim
+)
+
+//var geometryType = map[enumType]string{
+//	area:  "area",
+//	perim: "perim",
+//}
+
+func String(s enumType) string {
+	switch s {
+	case area:
+		return "Area"
+	case perim:
+		return "Perimeter"
+	default:
+		panic("no value assigned, we break out.")
+
+	}
+
+}
+
 func main() {
 
-	// enum 
-	type enumType int
+	fmt.Println(String(area))
 
-	const (
-		area = iota
-		perim
-	)
-
-	var geometryType = map[enumType]string{
-		area:  "area",
-		perim: "perim",
-	}
-
-	func (s enumType ) String() string{
-		
-		
-	}
 }
--- interface.go
+++ interface.go
@@ -8,7 +8,7 @@ import (
 // interface
 type Geometry interface {
 	area() float64
-	perim() float64
+	perimeter() float64
 }
 
 // struct
@@ -20,44 +20,70 @@ type Circle struct {
 	radius float64
 }
 
-//method calculate the area and perim for react and struct
-
+// method calculate the area and perimeter for react and struct
 func (r React) area() float64 {
+
+	fmt.Println("area of a rectangle")
 	return r.width * r.height
 }
-func (r React) perim() float64 {
+func (r React) perimeter() float64 {
 	result := 2*r.width + 2*r.height
+
+	fmt.Println("perimeter of a rectangle")
 
 	return math.Round(result)
 }
 
 func (c Circle) area() float64 {
 
-	fmt.Println("float64 value of area ==> roundedToEven", math.Pi*c.radius*c.radius)
-	return math.RoundToEven(math.Pi * c.radius * c.radius)
+	result := math.Pi * c.radius * c.radius
+	fmt.Println("float64 value of area ==> roundedToEven", result)
+	return math.RoundToEven(result)
 }
 
-func (c Circle) perim() float64 {
+func (c Circle) perimeter() float64 {
 	result := 2 * (math.Pi * c.radius)
-	fmt.Println("float64 value of area ==> rounded", result)
+	fmt.Println("float64 value of perimeter ==> rounded", result)
 
 	return math.Round(result)
 }
 
-//a function behalf as inheritor of the geometry interface
+// using enum to run a method
+type geoEnum int
 
-func measure(geometry Geometry) {
-	fmt.Println(geometry)
-	fmt.Println(geometry.area())
-	fmt.Println(geometry.perim())
+const (
+	runArea geoEnum = iota
+	runperimeter
+)
+
+// a function behalf as inheritor of the geometry interface
+func measure(geometry Geometry, enum geoEnum) {
+	fmt.Println("geometry value => ", geometry)
+
+	switch enum {
+	case runArea:
+		fmt.Println(geometry.area())
+		break
+	case runperimeter:
+		fmt.Println(geometry.perimeter())
+		break
+	default:
+		panic("Not Found!")
+	}
 }
 
 func main() {
 
 	rectangle := React{height: 10, width: 2}
-	measure(rectangle)
+	measure(rectangle, runperimeter)
 
 	circle := Circle{radius: 4}
-	measure(circle)
+	measure(circle, runperimeter)
 
+	fmt.Println("lets run for area")
+	rect := React{height: 10, width: 2}
+	measure(rect, runArea)
+
+	cir := Circle{radius: 4}
+	measure(cir, runArea)
 }
